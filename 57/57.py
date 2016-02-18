53d56bdfb455ee66b4d640fc6c973cf9848ee16d
Sam <samhong010@gmail.com>
2016-02-17 21:23:54 -0800

    Updated code to be more Pythonic

--- 57/57.py
+++ 57/57.py
@@ -2,9 +2,6 @@
 
 count=0;
 
-def swap(n,m):
-    return (m,n)
-
 def GetGCD(a,b):
     while (b != 0):
         r = a % b
@@ -12,24 +9,14 @@ def GetGCD(a,b):
         b = r
     return a
 
-def DigitCount(n):
-    count=0
-    while (n > 0):
-        count +=1
-        n //= 10
-    return count
-
 for expansion in range(0,1000):
     num=1
     denom=2;
     for i in range(0, expansion):
         num += denom*2;
-        num, denom = swap(num, denom)
+        num, denom = denom, num
     num += denom
-    gcd=GetGCD(num, denom)
-    num //= gcd
-    denom //= gcd
-    if (DigitCount(num) > DigitCount(denom)):
+    if (len(str(num)) > len(str(denom))):
         count += 1
 
 print (count)
