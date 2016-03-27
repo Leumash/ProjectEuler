3cedad9a93287e566f751b1bac2344142d54e0be
Sam <samhong010@gmail.com>
2016-03-26 23:52:23 -0700

    Adding input file

--- 63/63.py
+++ 63/63.py
@@ -4,13 +4,13 @@
 # len(str(10 ^ x)) = x + 1
 # Thus only bases < 10 are viable
 
+x = set({1,5})
+
 nDigitPositiveIntegersThatAreAlsoNthPower = set()
 
 for base in range(1, 10):
     for exponent in range(1, 30):
-        n = base ** exponent
-        if (len(str(n)) == exponent):
-            nDigitPositiveIntegersThatAreAlsoNthPower.add(n)
+        if (len(str(base ** exponent)) == exponent): nDigitPositiveIntegersThatAreAlsoNthPower.add(base ** exponent)
 
 
 print (len(nDigitPositiveIntegersThatAreAlsoNthPower))
