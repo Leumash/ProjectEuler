e9ff102470548345f79abae5d9b6e8d9b82b7ab8
Sam <samhong010@gmail.com>
2017-08-19 03:36:15 -0700

    Finished Problem 79

--- 79/79.cc
+++ 79/79.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,6 +13,7 @@ vector<string> readKeys() {
     }
     return keys;
 }
+
 bool isSubCode(string passcode, string key) {
     int pos;
     pos = passcode.find(key[0]);
@@ -34,15 +37,47 @@ bool isCorrectPasscode(string passcode, const vector<string> &keys) {
     return true;
 }
 
-int getPasscode(const vector<string> &keys) {
-    int passcode = stoi(keys[0]);
-    while (true) {
-        if (isCorrectPasscode(to_string(passcode), keys)) {
-            return passcode;
+vector<char> getRequiredDigits(const vector<string> &keys) {
+    unordered_set<char> requiredDigits;
+    for (string key : keys) {
+        for (char k : key) {
+            requiredDigits.insert(k);
         }
-        ++passcode;
+    }
+    vector<char> digits;
+    digits.insert(digits.end(), requiredDigits.begin(), requiredDigits.end());
+    return digits;
+}
+
+string convertToString(vector<char> keys) {
+    string toReturn;
+    for (int i=0; i<keys.size(); ++i) {
+        toReturn += keys[i];
+    }
+    return toReturn;
+}
+
+vector<string> getPermutations(vector<char> requiredKeys) {
+    vector<string> permutations;
+    sort(requiredKeys.begin(), requiredKeys.end());
+    do {
+        string permutation = convertToString(requiredKeys);
+        permutations.push_back(permutation);
+    } while (next_permutation(requiredKeys.begin(), requiredKeys.end()));
+    return permutations;
+}
+
+int getPasscode(const vector<string> &keys) {
+    vector<char> requiredKeys = getRequiredDigits(keys);
+    vector<string> permutations = getPermutations(requiredKeys);
+
+    for (string permutation : permutations) {
+        if (isCorrectPasscode(permutation, keys)) {
+            return stoi(permutation);
+        }
     }
 }
+
 
 int main() {
     vector<string> keys = readKeys();
