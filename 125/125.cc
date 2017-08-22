349ad5bfbc2eca5e7591a2a9eaaee59056e93021
Sam <samhong010@gmail.com>
2017-08-21 20:02:25 -0700

    Finished Problem 125
    Runs in 0.382s

--- 125/125.cc
+++ 125/125.cc
@@ -1,50 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
-vector<int> getValidSquares(int n) {
-    int upper = sqrt(n);
-    vector<int> validSquares;
-    for (int i=1; i<=upper; ++i) {
+vector<long long> getValidSquares(long long n) {
+    long long upper = sqrt(n);
+    vector<long long> validSquares;
+    for (long long i=1; i<=upper; ++i) {
         validSquares.push_back(i * i);
     }
     return validSquares;
 }
 
-vector<int> getPrefixSum(vector<int> nums) {
-    for (int i=1; i<nums.size(); ++i) {
+vector<long long> getPrefixSum(vector<long long> nums) {
+    for (long long i=1; i<nums.size(); ++i) {
         nums[i] += nums[i-1];
     }
     return nums;
 }
 
-bool isPalindrome(int n) {
+bool isPalindrome(long long n) {
     string num = to_string(n);
     string rev = num;
     reverse(rev.begin(), rev.end());
     return num == rev;
 }
 
-int getSumOfNumbersThatAreBothPalindromicAndSumOfConsecutiveSquares(int n) {
-    vector<int> squares = getValidSquares(n);
-    vector<int> prefixSum = getPrefixSum(squares);
+long long getSumOfNumbersThatAreBothPalindromicAndSumOfConsecutiveSquares(long long n) {
+    unordered_set<long long> nums;
+    vector<long long> squares = getValidSquares(n);
+    vector<long long> prefixSum = getPrefixSum(squares);
     prefixSum.insert(prefixSum.begin(), 0);
-    int sum = 0;
-    for (int b=1; b<prefixSum.size(); ++b) {
-        for (int a=0; a<b; ++a) {
-            int sumOfConsecutiveSquares = prefixSum[b] - prefixSum[a];
-            if (sumOfConsecutiveSquares < n && b - a > 1&& isPalindrome(sumOfConsecutiveSquares)) {
-                sum += sumOfConsecutiveSquares;
+    for (long long b=1; b<prefixSum.size(); ++b) {
+        for (long long a=0; a<b; ++a) {
+            long long sumOfConsecutiveSquares = prefixSum[b] - prefixSum[a];
+            if (sumOfConsecutiveSquares < n && b - a > 1 && isPalindrome(sumOfConsecutiveSquares)) {
+                nums.insert(sumOfConsecutiveSquares);
             }
         }
+    }
+    long long sum = 0;
+    for (long long num : nums) {
+        sum += num;
     }
     return sum;
 }
 
 int main() {
-    cout << getSumOfNumbersThatAreBothPalindromicAndSumOfConsecutiveSquares(1000) << endl;
-    return 0;
+    cout << getSumOfNumbersThatAreBothPalindromicAndSumOfConsecutiveSquares(100000000) << endl;
 }
 
