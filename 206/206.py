f26f91f91df263a8072bea9c1b9f6af2e39004e8
Sam <samhong010@gmail.com>
2017-09-11 01:05:28 -0700

    Finished Problem 206

--- 206/206.py
+++ 206/206.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+def getUsableFiveDigitNumbers():
+    l = []
+    for i in range(0, 100000):
+        squared = i * i
+        N = list(str(squared))
+        if len(N) >= 5 and N[-1] == '9' and N[-3] == '8' and N[-5] == '7':
+            l.append(str(i).zfill(5))
+    return l
+
 def doesFitForm(n):
     squared = n * n
     N = list(str(squared))
@@ -13,12 +22,15 @@ def doesFitForm(n):
         and N[14] == '8' \
         and N[16] == '9'
 
-def doIt():
-    i = 100000000
+def getConcealedSquare():
+    suffixes = getUsableFiveDigitNumbers()
+    i = 1000
     while True:
-        if doesFitForm(i):
-            return i * 10
-        i += 1
+        for suffix in suffixes:
+            num = str(i) + suffix
+            if doesFitForm(int(num)):
+                return num + '0'
+        i +=1
 
-print doIt()
+print getConcealedSquare()
 
